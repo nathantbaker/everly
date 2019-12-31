3b1765eea0a81253523120f15f66187d519dd075
Nathan T. Baker <nathantbaker@gmail.com>
2019-12-31 14:15:42 -0600

    basic settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `Team Everly`,
+    author: `Nate Baker`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `nathantbaker`,
     },
   },
   plugins: [
